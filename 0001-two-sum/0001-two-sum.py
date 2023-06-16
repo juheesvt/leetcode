14cedf6edde82159ca218ae97168a2b3a51fae8b
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2023-06-16 18:50:35 +0900

    Time: 1208 ms (35.82%), Space: 17.1 MB (64.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        answer = dict()
+        answer = list()
         for idx, value in enumerate(nums):
-
             if target-value in nums[idx+1:] or target-value in nums[0:idx]:
-                answer[idx] = value
-
-        return answer.keys()
+                answer.append(idx)
+        return answer
