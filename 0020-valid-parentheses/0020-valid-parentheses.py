b29b52846a7e0a5e1ac51efb70355ff37cda283c
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2023-06-17 16:27:52 +0900

    Time: 45 ms (62.91%), Space: 16.3 MB (43.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,25 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-
+        
         stack = list()
+        parentheses_dict = {'}': '{', ']': '[', ')': '('}
+        
         for parentheses in s:
-            if parentheses == ')':
-                if len(stack) and '(' == stack[-1]:
-                    del stack[-1]
-                else:
-                    return False
-            elif parentheses == ']':
-                if len(stack) and '[' == stack[-1]:
-                    del stack[-1]
-                else:
-                    return False
-
-            elif parentheses == '}':
-                if len(stack) and '{' == stack[-1]:
+            if parentheses in parentheses_dict.keys():
+                if stack and stack[-1] == parentheses_dict[parentheses]:
                     del stack[-1]
                 else:
                     return False
             else:
                 stack.append(parentheses)
-
+        print(stack)
         return True if len(stack) == 0 else False
