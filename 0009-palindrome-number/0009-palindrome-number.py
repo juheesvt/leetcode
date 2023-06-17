8faed7653b4684325795fef82adfd660da64953e
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2023-06-17 17:31:04 +0900

    Time: 65 ms (84.25%), Space: 16.2 MB (85.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,7 +3,8 @@ class Solution:
         if x < 0:
             return False
         
-        if str(x) == str(x)[::-1]:
-            return True
-        else:
-            return False
+        x = str(x)
+        for i in range(len(x)//2):
+            if x[i] != x[-i-1]:
+                return False
+        return True
